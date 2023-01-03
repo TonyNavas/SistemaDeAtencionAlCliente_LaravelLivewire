988e6adacfce93041544471200e6dfe146f98837
Tony Navas <tonynavas777@gmail.com>
2023-01-03 14:41:01 -0600

    Modificaciones

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RolController;
+use App\Http\Livewire\ProductComponent;
 use App\Http\Controllers\UserController;
+use App\Http\Livewire\CategoryComponent;
+use App\Http\Controllers\MessageController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,8 +27,17 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+// Notifications
+Route::post('messages', [MessageController::class,'store'])->name('messages.store');
+Route::get('messages/{message}',[ MessageController::class,'show'])->name('messages.show');
+
+
 
 Route::group(['middleware' => ['auth']], function(){
-    Route::resource('/admin/roles', RolController::class);
-    Route::resource('/admin/usuarios', UserController::class);
+    Route::resource('/admin/roles', RolController::class)->middleware('can:ver-roles');
+    Route::resource('/admin/usuarios', UserController::class)->middleware('can:ver-usuarios');
+
+    Route::get('admin/categories', CategoryComponent::class)->middleware('can:ver-categories');
+    Route::get('admin/products', ProductComponent::class);
 });
+
