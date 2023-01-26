9cd90cf3ff488ed6aa196f3c1a8a3a02ef11a55d
Tony Navas <tonynavas777@gmail.com>
2023-01-26 14:05:50 -0600

    Agregando el deploy.php

--- deploy.php
+++ deploy.php
@@ -2,71 +2,21 @@
 namespace Deployer;
 
 require 'recipe/laravel.php';
-require 'contrib/npm.php';
-require 'contrib/rsync.php';
 
-///////////////////////////////////
 // Config
-///////////////////////////////////
 
-set('application', 'Proyecto_Atencion_Al_Cliente');
-set('repository', 'git@github.com:TonyNavas/Proyecto_Atencion_Al_Cliente.git'); // Git Repository
-set('ssh_multiplexing', true);  // Speed up deployment
-//set('default_timeout', 1000);
+set('repository', 'git@github.com:TonyNavas/Proyecto_Atencion_Al_Cliente.git');
 
-set('rsync_src', function () {
-    return __DIR__; // If your project isn't in the root, you'll need to change this.
-});
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
 
-// Configuring the rsync exclusions.
-// You'll want to exclude anything that you don't want on the production server.
-add('rsync', [
-    'exclude' => [
-        '.git',
-        '/vendor/',
-        '/node_modules/',
-        '.github',
-        'deploy.php',
-    ],
-]);
-
-// Set up a deployer task to copy secrets to the server.
-// Grabs the dotenv file from the github secret
-task('deploy:secrets', function () {
-    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
-    upload('.env', get('deploy_path') . '/shared');
-});
-
-///////////////////////////////////
 // Hosts
-///////////////////////////////////
 
-host('prod') // Name of the server
-->setHostname('157.245.10.198') // Hostname or IP address
-->set('remote_user', 'root') // SSH user
-->set('branch', 'main') // Git branch
-->set('deploy_path', '/var/www/atencionAlCliente'); // Deploy path
+host('157.245.10.198')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/Proyecto_Atencion_Al_Cliente');
 
-after('deploy:failed', 'deploy:unlock');  // Unlock after failed deploy
+// Hooks
 
-///////////////////////////////////
-// Tasks
-///////////////////////////////////
-
-desc('Start of Deploy the application');
-
-task('deploy', [
-    'deploy:prepare',
-    'rsync',                // Deploy code & built assets
-    'deploy:secrets',       // Deploy secrets
-    'deploy:vendors',
-    'deploy:shared',        //
-    'artisan:storage:link', //
-    'artisan:view:cache',   //
-    'artisan:config:cache', // Laravel specific steps
-    'artisan:migrate',      //
-    'artisan:queue:restart',//
-    'deploy:publish',       //
-]);
-
-desc('End of Deploy the application');
+after('deploy:failed', 'deploy:unlock');
